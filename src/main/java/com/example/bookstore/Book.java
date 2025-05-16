312ad1afdf5df092a00349730c3421a0ffdbf1bb
Craig Derington <craig@craigderington.dev>
2025-05-16 13:51:31 -0400

    Create constructors for the Book object.

--- src/main/java/com/example/bookstore/Book.java
+++ src/main/java/com/example/bookstore/Book.java
@@ -2,22 +2,26 @@ package com.example.bookstore;
 
 public class Book {
 
-    private String id;
-    private String title;
-    private String author;
+    String id;
+    String title;
+    String author;
 
+    // default constructor
     public Book() {}
 
+    // main constructor
     public Book(String id, String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
     }
 
+    // getter
     public String getId() {
         return this.id;
     }
 
+    // setter
     public void setId(String id) {
         this.id = id;
     }
